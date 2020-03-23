ad46385bff4eb4e2653955ba368ebdf218b1bbca
Maftuna Khakimova <sobira204@gmail.com>
2020-03-23 10:46:51 +0500

    create new project

---
+++ NewFile/Program.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+namespace NewFile
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+            Console.ReadKey();
+        }
+    }
+}
